ceff16681e1fd27dcefc823d84075f6d2b74b526
coderrooster <abitohari@gmail.com>
2020-06-30 19:19:19 +0700

    show list data post in home page

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="container">
-    <div class="jumbotron">
-      <h1 class="display-4">Hello, world!</h1>
-      <p class="lead">This is home page for simple bog web app build with Laravel v7 and Vue.js v2.</p>
-      <hr class="my-4" />
-      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-      <!-- <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> -->
-      <router-link class="btn btn-primary btn-lg" :to="{name: 'post.create'}">Let's Create New Post</router-link>
+
+    <div class="row">
+
+      <div v-for="(post, index) in posts" :key="index" class="col-sm-12 col-md-4">
+        <div class="card mb-4">
+          <img src="assets/images/beach.jpg" class="card-img-top" alt="image post" />
+          <div class="card-body">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <p class="card-text">{{ post.body }}</p>
+            <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+          <div class="card-footer">
+            <strong>Category : {{ post.category }}</strong> | 
+            <small class="text-muted text-right right-align">Published : {{ post.publishedForHuman }}</small>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="posts.length < 1" class="col-md-12">
+        <div class="alert alert-info" role="alert">
+          Sorry data post is empty! Please <router-link class="alert-link" :to="{name: 'post.create'}">create new post</router-link>.
+        </div>
+      </div>
+
     </div>
+
   </div>
 </template>
 
 <script>
-export default {};
-</script>
+  export default {
+    data() {
+      return {
+        posts: []
+      };
+    },
 
-<style>
-</style>
+    mounted() {
+      axios.get("api/posts/").then(res => {
+        this.posts = res.data.data;
+        if(res.data.data.length < 1){
+          console.log('post is empty!')
+        }
+      });
+    }
+  };
+</script>
